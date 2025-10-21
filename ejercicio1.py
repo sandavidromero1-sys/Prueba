bcdfc4cc75ca9bd752ac4b3f0bb725b7c4300a72
sandavidromero1-sys <sandavidromero1@gmail.com>
2025-10-20 21:49:41 -0500

    Cambio en el ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,29 @@
 nombre= input("Ingrese su nombre: ")
 print("Tu nombre es:", nombre)
+
+nombre= "Juan"
+telefono= "123456789"
+direccion= "Calle Falsa 123"
+edad=20
+
+print("Mi nombre es:" + nombre + ", mi telefono es:" + telefono + ", mi direccion es:" + direccion + ", y mi edad es:" + str(edad))
+print(f"Mi nombre es: {nombre}, mi telefono es: {telefono}, mi direccion es: {direccion}, y mi edad es: {edad}")
+
+#datos enteros 
+numero = 0
+numero_decimal = 0.0
+saludo = "Hola Mundo"
+datos_tipo_logico = True
+
+#operadores aritmeticos
+a=8
+b=10
+c,d=8,10
+print(f"Suma: {a+b}")
+print(f"suma: {c+d}")
+#desarrollar  - , *,/, //,**
+print (f"Resta: {a-b}")
+print (f"Multiplicacion: {a*b}")
+print (f"Division: {b/a}")
+print (f"Division entera: {b//a}")
+print (f"Exponente: {a**2}")
